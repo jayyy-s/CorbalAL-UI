766731477a57c79ff97d7c0263456b86ec754131
jayyy-s <srinivasan.j@northeastern.edu>
2022-03-10 16:46:57 -0500

    bids view all link and analytics

--- corbal-react-app/src/components/YourBids.jsx
+++ corbal-react-app/src/components/YourBids.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import "./css/yourBids.css";
+import { Link } from "react-router-dom";
 
 function YourBids(props) {
+
+  const viewAll = [
+    {
+    key: "home",
+    link: "/",
+  }
+];
 
   return (
     <div class="YourBids">
@@ -10,12 +18,24 @@ function YourBids(props) {
           <div class="your-bids-text">Your Bids</div>
           <div class="bids-divider"></div>
         </div>
-        <div class="col-md-4">
-          <div class="view-all-button">View All</div>
+        <div class="col-md-2">
+          {viewAll.map(({ key, link }) => {
+            return (
+              <div className="view-all-button">
+                <Link className="view-all-link" to={link} key={key}>View All</Link> 
+              </div>
+            );
+          })}
         </div>
         {/* not sure how to format this part correctly*/}
-        <div class="col-md-4">
-          <div class="bid-analytics">{props.numCompleted} Completed {props.numPending} Pending {props.numUnread} Unread</div>
+        <div class="col-md-2">
+          <div class="bid-analytics">{props.numCompleted} Completed</div>
+        </div>
+        <div class="col-md-2">
+          <div class="bid-analytics">{props.numPending} Pending</div>
+        </div>
+        <div class="col-md-2">
+          <div class="bid-analytics">{props.numUnread} Unread</div>
         </div>
       </div>
     </div>
