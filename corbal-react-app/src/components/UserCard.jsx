7e1d5b77d48b15a6f8b5196c8fc15045c8700630
oshoura <omarshoura1@gmail.com>
2022-03-23 14:33:34 -0400

    Fixed header and user card

--- corbal-react-app/src/components/UserCard.jsx
+++ corbal-react-app/src/components/UserCard.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import "./css/userCard.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function UserCard(props) {
-
   return (
     <div class="UserCard">
-      <div class="row">
-        <div class="col-md-1">
-          <div className="pfp-container">
-            <img src={props.src} alt='where did ur prof pic go xD' />
-          </div>
+      <div className="profile-container">
+        <div className="pfp-container">
+          <img src={props.src} alt="where did ur prof pic go xD" />
         </div>
-        <div class="col-md-6">
+        <div class="content-container">
           <div className="username">
             <b>{props.username}</b>
           </div>
           <div className="real-name-age">
             {props.realName} ({props.age})
           </div>
-          <div class="user-detail">
-            {props.genre}
-          </div>
+          <div class="user-detail">{props.genre}</div>
           <div class="user-detail">
             {props.location} | {props.email} | {props.phone}
+          </div>
+        </div>
+      </div>
+      <div className="pitch-wrapper">
+        <div className="pitch-container">
+          <p className="pitch-title">Pitch a Song</p>
+          <div className="pitch-button-container">
+            <p>+</p>
           </div>
         </div>
       </div>
