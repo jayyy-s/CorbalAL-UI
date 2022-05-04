a9aa00e37f69cba80cf08f463b736f3be9b8a6a1
jayyy-s <srinivasan.j@northeastern.edu>
2022-05-04 09:46:05 -0400

    tab icon and title

--- corbal-react-app/src/utilities/constants.js
+++ corbal-react-app/src/utilities/constants.js
@@ -1,3 +1,3 @@
 export const GENRES=["Pop","EDM","ROCK"];
 
-export const spotifyToken = "BQDHwikxr18g4JsStXznom5tb0HKISfkcjKVXJ0wAVLimTsPEHHV24HOggwRyRomxf97Lv18uggqRHC5hOBwIKRZQuNjrad47j1Zhd-UvhAHOAMmtB-2zeI6QQ1AxMSjyej8sQVXvm6q-YwgvRXjjOC8u9wInPLo5QorPQg";
+export const spotifyToken = "BQCtTxdUL3D6uMRC81or_xlq94E7aD2_5O65MbbA4fPPFFExCF1IVnx2j_tbEbYuTW05b_ps0I-x6nCpAjmQQ-lqCv7astKWqrToDwzS8TKxagW6Ihhbe0mrZ3jgG5sdyewzIIUV1gHiwhDmor7K"
